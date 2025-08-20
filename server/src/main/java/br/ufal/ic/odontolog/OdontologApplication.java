5fb12a27439be816954b2437e1e48a353d962a34
Ricardo <ricardoviniciusaf@gmail.com>
2025-08-20 17:04:14 -0300

    chore(server): Add initial project structure with Maven configuration and Spring Boot setup

---
+++ server/src/main/java/br/ufal/ic/odontolog/OdontologApplication.java
@@ -0,0 +1,13 @@
+package br.ufal.ic.odontolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontologApplication.class, args);
+	}
+
+}
