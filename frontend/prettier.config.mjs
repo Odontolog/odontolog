4248638429594689ebebd6c61287e5db26dc3de7
Arthur Soares <arthurpmrs@gmail.com>
2025-09-03 13:58:21 -0300

    chore(frontend): init frontend folder

---
+++ frontend/prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  printWidth: 80,
+};
+
+export default config;
