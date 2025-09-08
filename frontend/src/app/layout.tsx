ade41d9043258fe2462cc7b7a44f00ce3e0e60df
BeaRoodriguees <beatrizroodriguees@hotmail.com>
2025-09-08 18:11:04 -0300

    feat(sidebar): Implement sidebar component with navigation links

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import '@mantine/core/styles.css';
 import '@mantine/spotlight/styles.css';
 
-import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
+import { ColorSchemeScript, Flex, mantineHtmlProps } from '@mantine/core';
 
 import Providers from './providers';
 import Navbar from '@/features/appshell/navbar';
+import { Sidebar } from '@/features/appshell/sidebar';
 
 export const metadata = {
   title: 'Odontolog',
@@ -23,8 +24,13 @@ export default function RootLayout({
       </head>
       <body>
         <Providers>
-          <Navbar />
-          {children}
+          <Flex>
+            <Sidebar />
+            <div style={{ flex: 1 }}>
+              <Navbar />
+              {children}
+            </div>
+          </Flex>
         </Providers>
       </body>
     </html>
---
+++ frontend/src/features/appshell/sidebar.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { useState } from 'react';
+import {
+  IconBook,
+  //   IconBuildingHospital,
+  IconDental,
+  IconLogout,
+  IconUsers,
+} from '@tabler/icons-react';
+import { Center, Divider, Stack, Tooltip, UnstyledButton } from '@mantine/core';
+import classes from './sidebar.module.css';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+interface NavbarLinkProps {
+  icon: typeof IconDental;
+  label: string;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      <UnstyledButton
+        onClick={onClick}
+        className={classes.link}
+        data-active={active || undefined}
+      >
+        <Icon size={22} stroke={2} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+const mockdata = [
+  //   { icon: IconBuildingHospital, label: 'Clínicas', route: '' },
+  { icon: IconDental, label: 'Pacientes', route: '/patients' },
+  { icon: IconUsers, label: 'Alunos', route: '/students' },
+  { icon: IconBook, label: 'Revisões', route: '/validation' },
+];
+
+export function Sidebar() {
+  const [active, setActive] = useState(2);
+  const router = useRouter();
+
+  const links = mockdata.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => {
+        setActive(index);
+        router.push(link.route);
+      }}
+    />
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <Center>
+        <Image
+          src="/odontolog.svg"
+          alt="Odontolog Logo"
+          width={40}
+          height={40}
+          style={{ marginBottom: '0.5rem' }}
+        />
+      </Center>
+
+      <Divider
+        style={{ borderTopColor: 'var(--mantine-color-default-border)' }}
+      />
+
+      <div className={classes.navbarMain}>
+        <Stack justify="center" gap={4}>
+          {links}
+        </Stack>
+      </div>
+
+      <Stack justify="center" gap={0}>
+        <NavbarLink
+          icon={IconLogout}
+          label="Sair da conta"
+          onClick={() => {
+            console.log('Fazer logout');
+          }}
+        />
+      </Stack>
+    </nav>
+  );
+}
