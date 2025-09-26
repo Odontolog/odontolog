f893d3fc3f01852b7accf9de8043564e867d5b7e
Ricardo <ricardoviniciusaf@gmail.com>
2025-09-26 16:45:28 -0300

    feat: Implement ReviewableApi interface and OpenApiConfig for API documentation and security

---
+++ server/src/main/java/br/ufal/ic/odontolog/api/ReviewableApi.java
@@ -0,0 +1,34 @@
+package br.ufal.ic.odontolog.api;
+
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.ufal.ic.odontolog.dtos.ReviewableCurrentSupervisorFilterDTO;
+import br.ufal.ic.odontolog.dtos.ReviewableDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Reviewables", description = "Endpoints for managing reviewable items")
+@SecurityRequirement(name = "bearerAuth")
+public interface ReviewableApi {
+
+    @Operation(summary = "Fetch reviewable items for the logged-in supervisor", description = "Returns a paginated list of items that are awaiting review or belong to the authenticated supervisor, based on the provided filters.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search completed successfully."),
+            @ApiResponse(responseCode = "422", description = "Authenticated supervisor not found."),
+    })
+    public ResponseEntity<PagedModel<ReviewableDTO>> getCurrentSupervisorReviewables(
+            @ParameterObject Pageable pageable,
+            @ParameterObject ReviewableCurrentSupervisorFilterDTO filter,
+            @Parameter(hidden = true) UserDetails currentUser);
+}
--- server/src/main/java/br/ufal/ic/odontolog/controllers/ReviewableController.java
+++ server/src/main/java/br/ufal/ic/odontolog/controllers/ReviewableController.java
@@ -1,11 +1,8 @@
 package br.ufal.ic.odontolog.controllers;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ufal.ic.odontolog.api.ReviewableApi;
 import br.ufal.ic.odontolog.dtos.ReviewableCurrentSupervisorFilterDTO;
 import br.ufal.ic.odontolog.dtos.ReviewableDTO;
 import br.ufal.ic.odontolog.services.ReviewableService;
@@ -21,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RestController()
 @RequestMapping("/api/reviewables")
 @RequiredArgsConstructor
-public class ReviewableController {
+public class ReviewableController implements ReviewableApi {
     private final ReviewableService reviewableService;
 
     @PreAuthorize("hasRole('SUPERVISOR')")
