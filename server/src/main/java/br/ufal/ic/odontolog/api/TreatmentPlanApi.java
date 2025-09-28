dbe7c1d204233589af9ccba212884d5450299d0d
Ricardo <ricardoviniciusaf@gmail.com>
2025-09-28 17:43:18 -0300

    refactor: Enhance documentation for assignUserToTreatmentPlan method in TreatmentPlanApi

--- server/src/main/java/br/ufal/ic/odontolog/api/TreatmentPlanApi.java
+++ server/src/main/java/br/ufal/ic/odontolog/api/TreatmentPlanApi.java
@@ -16,21 +16,23 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @SecurityRequirement(name = "bearerAuth")
 public interface TreatmentPlanApi {
 
-    @Operation(summary = "Assign user to treatment plan", description = """
-            Assigns a user to a specific treatment plan.
-            Preconditions:
-            - The treatment plan must be in the 'DRAFT' state.
-            - The treatment plan must exist in the system.
-            - The user must exist in the system.
+        @Operation(summary = "Assign user to treatment plan", description = """
+                        Assigns a user to a specific treatment plan.
 
-            Postconditions: The treatment plan will be assigned to the specified user.""")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User assigned successfully."),
-            @ApiResponse(responseCode = "422", description = "Invalid request."),
-            @ApiResponse(responseCode = "404", description = "Treatment Plan not found."),
-            @ApiResponse(responseCode = "400", description = "Treatment Plan is not in a state that allows user assignment.")
-    })
-    public ResponseEntity<TreatmentPlanDTO> assignUserToTreatmentPlan(
-            @RequestBody TreatmentPlanAssignUserRequestDTO requestDTO,
-            @Parameter Long treatment_id);
+                        Preconditions:
+                        - The treatment plan must be in the 'DRAFT' state.
+                        - The treatment plan must exist in the system.
+                        - The user must exist in the system.
+
+                        Postconditions:
+                        - The treatment plan will be assigned to the specified user.""")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "User assigned successfully."),
+                        @ApiResponse(responseCode = "422", description = "Invalid request."),
+                        @ApiResponse(responseCode = "404", description = "Treatment Plan not found."),
+                        @ApiResponse(responseCode = "400", description = "Treatment Plan is not in a state that allows user assignment.")
+        })
+        public ResponseEntity<TreatmentPlanDTO> assignUserToTreatmentPlan(
+                        @RequestBody TreatmentPlanAssignUserRequestDTO requestDTO,
+                        @Parameter Long treatment_id);
 }
