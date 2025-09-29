5a3f71a811061fb45bff563d7fdf8691130aeb15
Bea Rodrigues <116564168+BeaRoodriguees@users.noreply.github.com>
2025-09-29 11:11:16 -0300

    feat(frontend): Listagem das validações pendentes de um professor (#22)
    * feat(frontend): implement validations section and related components for procedure and treatment plan review
    
    * feat(frontend): enhance validations section with scrollable content and dynamic badge count
    
    * feat(frontend): add disableSession prop to ProcedureCard to conditionally render session info
    
    * feat(frontend): update ProcedureValidationPage to mock with TreatmentPlanDetailSection component
    
    * feat(frontend): fix asked changes
    
    * fix(sidebar): format sidebar link for validation requests

---
+++ frontend/src/features/validations/validations-section.tsx
@@ -0,0 +1,128 @@
+'use client';
+import {
+  Badge,
+  Card,
+  Center,
+  Divider,
+  Group,
+  ScrollArea,
+  Skeleton,
+  Stack,
+  Text,
+} from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import ProcedureCard from '@/shared/components/procedure-card';
+import TreatmentPlanCard from '@/shared/components/treatment-plan-card';
+import {
+  ProcedureShort,
+  ReviewableShort,
+  TreatmentPlanShort,
+} from '@/shared/models';
+import { getValidationsOptions } from './requests';
+
+export default function ValidationsSection() {
+  const options = getValidationsOptions();
+
+  const { data, isLoading } = useQuery({ ...options });
+
+  return (
+    <Card withBorder shadow="sm" radius="md" px="sm" h="100%" miw="400px">
+      <Card.Section inheritPadding py="sm">
+        <Group justify="space-between">
+          <Group>
+            <Text fw={600} size="lg">
+              Validação
+            </Text>
+            {data && (
+              <Badge variant="light" color="yellow" size="md">
+                {data?.length} esperando
+              </Badge>
+            )}
+          </Group>
+        </Group>
+      </Card.Section>
+
+      <Divider my="none" />
+
+      <Card.Section inheritPadding px="md" py="sm" h="100%">
+        {isLoading || data === undefined ? (
+          <Stack h="100%" gap="xs">
+            <Skeleton height={120} radius="none" />
+            <Skeleton height={120} radius="none" />
+            <Skeleton height={120} radius="none" />
+            <Skeleton height={120} radius="none" />
+            <Skeleton height={120} radius="none" />
+          </Stack>
+        ) : (
+          <ScrollArea scrollbarSize={6} w="100%" h="600px">
+            <ValidationsContent data={data} />
+          </ScrollArea>
+        )}
+      </Card.Section>
+    </Card>
+  );
+}
+
+function isProcedure(r: ReviewableShort): r is ProcedureShort {
+  return r.type === 'procedure';
+}
+
+function isTreatmentPlan(r: ReviewableShort): r is TreatmentPlanShort {
+  return r.type === 'treatment_plan';
+}
+
+function ValidationsContent({ data }: { data: ReviewableShort[] }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const active = searchParams.get('active');
+
+  function onReviewableSelect(reviewableId: string) {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set('active', reviewableId);
+    router.push(`?${newParams.toString()}`, { scroll: false });
+  }
+
+  if (data.length === 0) {
+    return (
+      <Center py="md" h="100%" px="lg">
+        <Stack align="center">
+          <Text fw={500} size="lg" c="dimmed" ta="center">
+            Não há procedimentos ou planos de tratamento precisando de sua
+            revisão
+          </Text>
+        </Stack>
+      </Center>
+    );
+  }
+
+  return (
+    <Stack>
+      {data.map((rev, index) => {
+        if (isProcedure(rev)) {
+          return (
+            <ProcedureCard
+              key={index}
+              procedure={rev}
+              selected={rev.id === active?.toString()}
+              onSelect={onReviewableSelect}
+              disableSession
+            />
+          );
+        }
+        if (isTreatmentPlan(rev)) {
+          return (
+            <TreatmentPlanCard
+              key={index}
+              treatmentPlan={rev}
+              selected={rev.id === active?.toString()}
+              onSelect={onReviewableSelect}
+            />
+          );
+        }
+        return null;
+      })}
+    </Stack>
+  );
+}
