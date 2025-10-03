23b6903d2652dbc446d371b105f84acb5ef082ae
Faite100 <coelhohugo004@gmail.com>
2025-10-03 16:15:57 -0300

    feat: Add student core funcionalities

---
+++ server/src/main/java/br/ufal/ic/odontolog/mappers/StudentMapper.java
@@ -0,0 +1,14 @@
+package br.ufal.ic.odontolog.mappers;
+
+import br.ufal.ic.odontolog.dtos.StudentDTO;
+import br.ufal.ic.odontolog.models.Student;
+import java.util.List;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface StudentMapper {
+
+  StudentDTO toDTO(Student student);
+
+  List<StudentDTO> toDTOs(List<Student> students);
+}
