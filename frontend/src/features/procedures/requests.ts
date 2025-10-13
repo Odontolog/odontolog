7e818ffe5ee965b83b00494ca64e20e286d654fa
Gabriel Gomes de Oliveira <86529029+KeeOtth@users.noreply.github.com>
2025-10-13 12:38:54 -0300

    PDS-160 feat(frontend): Criados os componentes de datas para última sessão e marcação da próxima. (#58)
    * iniciada a criação do componente das datas do histórico de consultas do paciente
    
    * feito componente de marcação de próximas datas.
    
    * minor changes in style
    
    * fix: dates of the demo
    
    * added skeleton on lastConsultationDate
    
    * removed the info icon on the left card
    
    ---------
    
    Co-authored-by: Arthur Soares <arthurpmrs@gmail.com>

--- frontend/src/features/procedures/requests.ts
+++ frontend/src/features/procedures/requests.ts
@@ -17,3 +17,30 @@ async function getPatientProcedures(
   await new Promise((resolve) => setTimeout(resolve, 300));
   return proceduresShort.filter((procedure) => procedure.status === 'DONE');
 }
+
+export function getNextConsultationDate(patientId: string) {
+  return queryOptions({
+    queryKey: ['nextConsultationDate', patientId],
+    queryFn: () => getNextConsultation(patientId),
+  });
+}
+
+export async function getNextConsultation(patientId: string) {
+  console.log(
+    `Fetching data for patient's id ${patientId} next consultation date.`,
+  );
+  await new Promise((resolve) => setTimeout(resolve, 300));
+  return { success: true };
+}
+
+export async function saveNextConsultation(
+  date: Date | undefined,
+  patientId: string,
+) {
+  console.log(JSON.stringify({ date }));
+  console.log(
+    `Next consultation on date ${date ? date.toISOString() : 'undefined'} saved for patient ${patientId}`,
+  );
+  await new Promise((resolve) => setTimeout(resolve, 300));
+  return { success: true };
+}
