e0309734995b9812023a3ae0c3298f334825a880
Davi Romão <davi.romao0210@gmail.com>
2025-10-20 21:09:28 -0300

    create anamnese models

---
+++ server/src/main/java/br/ufal/ic/odontolog/models/Anamnese.java
@@ -0,0 +1,35 @@
+package br.ufal.ic.odontolog.models;
+
+import jakarta.persistence.*;
+import java.util.Set;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor()
+@Table(name = "anamneses")
+public class Anamnese {
+  @Id private Long id;
+
+  @OneToMany(
+      mappedBy = "anamnese",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true,
+      fetch = FetchType.LAZY)
+  @Builder.Default
+  private Set<AnamneseActivity> history = new java.util.HashSet<>();
+
+  @OneToMany(
+      mappedBy = "anamnese",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true,
+      fetch = FetchType.LAZY)
+  @Builder.Default
+  private Set<PatientCondition> conditions = new java.util.HashSet<>();
+}
---
+++ server/src/main/java/br/ufal/ic/odontolog/models/PatientCondition.java
@@ -0,0 +1,25 @@
+package br.ufal.ic.odontolog.models;
+
+import br.ufal.ic.odontolog.enums.ClinicalCondition;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "patient_conditions")
+public class PatientCondition {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "anamnese_id", nullable = false)
+  private Anamnese anamnese;
+
+  @Enumerated(EnumType.STRING)
+  private ClinicalCondition condition;
+
+  private String notes;
+
+  private String category;
+
+  private Boolean hasCondition;
+}
