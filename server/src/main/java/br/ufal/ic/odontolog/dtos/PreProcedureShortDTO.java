7e2bc18bcd3bb6a3afae127f2a90a31c08bb6a82
Faite100 <coelhohugo004@gmail.com>
2025-10-22 10:24:57 -0300

    chores: comment removed

--- server/src/main/java/br/ufal/ic/odontolog/dtos/PreProcedureShortDTO.java
+++ server/src/main/java/br/ufal/ic/odontolog/dtos/PreProcedureShortDTO.java
@@ -7,13 +7,4 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class PreProcedureShortDTO extends ProcedureShortDTO {
-  //    private Long id;
-  //    private ProcedureStatus status;
-  //    private String name;
-  //    private String notes;
-  //    private UserResponseDTO assignee;
-  //    private PatientShortDTO patient;
-  //    private Instant updatedAt;
-  //    private Boolean deleted;
-}
+public class PreProcedureShortDTO extends ProcedureShortDTO { }
