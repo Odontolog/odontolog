fe2f9577941bf26babe4306748b561d66741611a
Ricardo <ricardoviniciusaf@gmail.com>
2025-10-21 22:03:13 -0300

    feat: add Terraform setup script and update localstack endpoint

--- infra/provider-local.tf
+++ infra/provider-local.tf
@@ -12,13 +12,13 @@ provider "aws" {
 
   access_key = "test"
   secret_key = "test"
-  
+
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    s3     = "http://localhost:4566"
+    s3     = "http://localstack-main:4566"
   }
 }
