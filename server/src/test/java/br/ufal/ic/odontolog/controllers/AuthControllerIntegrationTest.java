720f83b51a5b87d8e3e7a1f5133f6117f5af626f
Davi Romão <davi.romao0210@gmail.com>
2025-10-22 18:50:43 -0300

    chore: spotless apply

--- server/src/test/java/br/ufal/ic/odontolog/controllers/AuthControllerIntegrationTest.java
+++ server/src/test/java/br/ufal/ic/odontolog/controllers/AuthControllerIntegrationTest.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
