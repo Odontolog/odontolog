08b38f2ea54467356c14045f9a7280379be055cf
Arthur Soares <arthurpmrs@gmail.com>
2025-10-27 17:15:17 -0300

    feat: add new patient btn to patient page

--- frontend/src/app/(main)/patients/page.tsx
+++ frontend/src/app/(main)/patients/page.tsx
@@ -1,10 +1,13 @@
 import { Box } from '@mantine/core';
 
 import { getAllPatients } from '@/features/appshell/requests';
+import CreatePatientButton from '@/features/patients/create-patient-button';
 import PatientsSection from '@/features/patients/patients';
 import Header from '@/shared/components/header';
+import { requireAuth } from '@/shared/utils';
 
 export default async function PatientsListPage() {
+  const user = await requireAuth();
   const patients = await getAllPatients();
 
   return (
@@ -12,6 +15,7 @@ export default async function PatientsListPage() {
       <Header
         title="Pacientes"
         subtitle="Veja abaixo a listagem dos pacientes cadastrados no sistema."
+        {...(user.role !== 'STUDENT' && { button: <CreatePatientButton /> })}
       />
       <Box py="md" px="lg" h="100%">
         <PatientsSection patients={patients} />
