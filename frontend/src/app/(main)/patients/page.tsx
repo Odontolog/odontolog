d1464f6947d37717b7baf8853da0b72afb92033d
Arthur Soares <arthurpmrs@gmail.com>
2025-10-27 16:43:28 -0300

    feat: add patient page

--- frontend/src/app/(main)/patients/page.tsx
+++ frontend/src/app/(main)/patients/page.tsx
@@ -1,40 +1,21 @@
-'use client';
+import { Box } from '@mantine/core';
 
-import { Card, Center, Image, Stack, Text } from '@mantine/core';
+import { getAllPatients } from '@/features/appshell/requests';
+import PatientsSection from '@/features/patients/patients';
+import Header from '@/shared/components/header';
 
-export default function PatientsListPage() {
+export default async function PatientsListPage() {
+  const patients = await getAllPatients();
+
   return (
-    <Center h="100%" w="100%">
-      <Card
-        shadow="sm"
-        withBorder
-        radius="md"
-        p="xl"
-        style={{
-          maxWidth: 600,
-          textAlign: 'center',
-          backgroundColor: '#fff',
-        }}
-      >
-        <Stack align="center" gap="md">
-          <Image
-            src="/working.png"
-            alt="Em desenvolvimento"
-            maw={300}
-            radius="md"
-            style={{ opacity: 0.9 }}
-          />
-
-          <Text fw={700} size="xl" c="dark">
-            Listagem de Pacientes
-          </Text>
-
-          <Text c="dimmed" size="md">
-            Estamos trabalhando para que essa listagem esteja pronta o mais cedo
-            possível. Volte em breve!
-          </Text>
-        </Stack>
-      </Card>
-    </Center>
+    <>
+      <Header
+        title="Pacientes"
+        subtitle="Veja abaixo a listagem dos pacientes cadastrados no sistema."
+      />
+      <Box py="md" px="lg" h="100%">
+        <PatientsSection patients={patients} />
+      </Box>
+    </>
   );
 }
