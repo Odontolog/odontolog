9e53f4c34430dced8e208a19f55e773188df97c0
Arthur Soares <31834336+Arthurpmrs@users.noreply.github.com>
2025-10-27 13:38:28 -0300

    [PDS-170] feat(anamnese): Cria página da anamnese (#95)
    * feat(anamnese): add basic anamnese page structure
    
    * fix: make whole subpage scroll
    
    * fix(anamnese): fix sizes and add responsiveness
    
    * feat: add save request to anamnese section
    
    * feat: add history section to anamnese
    
    * feat: make switch colorful when checked even on disabled mode

---
+++ frontend/src/features/anamnese/ui/anamnese-section.tsx
@@ -0,0 +1,214 @@
+'use client';
+
+import {
+  ActionIcon,
+  Button,
+  Card,
+  Center,
+  Divider,
+  Flex,
+  Grid,
+  Group,
+  Loader,
+  Text,
+  ThemeIcon,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { notifications } from '@mantine/notifications';
+import { IconEdit, IconExclamationCircle } from '@tabler/icons-react';
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import { useState } from 'react';
+
+import PatientConditionCard from '@/features/anamnese/ui/condition-card';
+import { Anamnese, AnamneseFormValues, PatientCondition } from '../models';
+import { saveAnamnese } from '../requests';
+
+interface AnamneseSectionProps {
+  patientId: string;
+  queryOptions: UseQueryOptions<Anamnese, Error, Anamnese, string[]>;
+}
+
+export default function AnamneseSection(props: AnamneseSectionProps) {
+  const { patientId, queryOptions } = props;
+  const [editing, setEditing] = useState(false);
+
+  const {
+    data: conditions,
+    isLoading,
+    isError,
+  } = useQuery({
+    ...queryOptions,
+    select: (data) => data.conditions,
+    enabled: true,
+  });
+
+  return (
+    <Card withBorder shadow="sm" radius="md" px="sm" flex="1">
+      <Card.Section inheritPadding py="sm">
+        <Group justify="space-between">
+          <Text fw={600} size="lg">
+            Anamnese
+          </Text>
+          {!editing && (
+            <ActionIcon
+              variant="subtle"
+              color="gray"
+              disabled={isLoading || isError}
+              onClick={() => setEditing(true)}
+            >
+              <IconEdit size={16} />
+            </ActionIcon>
+          )}
+        </Group>
+      </Card.Section>
+
+      <Divider my="none" />
+
+      <Card.Section
+        inheritPadding
+        px="md"
+        py="sm"
+        flex="1"
+        h="100%"
+        style={{ overflowY: 'hidden' }}
+      >
+        <AnamneseSectionContent
+          patientId={patientId}
+          queryOptions={queryOptions}
+          isError={isError}
+          isLoading={isLoading}
+          editing={editing}
+          conditions={conditions}
+          setEditing={setEditing}
+        />
+      </Card.Section>
+    </Card>
+  );
+}
+
+interface AnamneseSectionContent extends AnamneseSectionProps {
+  conditions?: PatientCondition[];
+  isLoading: boolean;
+  isError: boolean;
+  editing: boolean;
+  setEditing: (value: boolean) => void;
+}
+
+function AnamneseSectionContent(props: AnamneseSectionContent) {
+  const {
+    patientId,
+    conditions,
+    isError,
+    isLoading,
+    editing,
+    setEditing,
+    queryOptions,
+  } = props;
+
+  const initialConditions: AnamneseFormValues = {
+    conditions: [],
+  };
+
+  if (conditions) {
+    initialConditions.conditions = conditions.map((condition) => ({
+      notes: condition.notes,
+      hasCondition: condition.hasCondition,
+    }));
+  }
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (anamnese: AnamneseFormValues) =>
+      saveAnamnese(patientId, anamnese),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: queryOptions.queryKey,
+      });
+      setEditing(false);
+    },
+    onError: (error) => {
+      notifications.show({
+        title: 'Não foi possível salvar os dados',
+        message: `Um erro inesperado aconteceu e não foi possível salvar suas alterações . Tente novamente mais tarde. ${error}`,
+        color: 'red',
+        icon: <IconExclamationCircle />,
+        autoClose: 5000,
+      });
+    },
+  });
+
+  const form = useForm<AnamneseFormValues>({
+    mode: 'uncontrolled',
+    initialValues: initialConditions,
+  });
+
+  if (isLoading) {
+    return (
+      <Center py="md">
+        <Loader size="sm" />
+      </Center>
+    );
+  }
+
+  if (isError || conditions === undefined || conditions.length === 0) {
+    return (
+      <Flex align="center" gap="xs">
+        <ThemeIcon variant="white" color="red">
+          <IconExclamationCircle size={24} />
+        </ThemeIcon>
+        <Text size="sm" c="red" py="none">
+          Erro ao carregar formulário de anamnese do paciente
+        </Text>
+      </Flex>
+    );
+  }
+
+  function handleCancel() {
+    setEditing(false);
+    form.reset();
+    mutation.reset();
+  }
+
+  function handleSubmit(values: AnamneseFormValues) {
+    mutation.mutate(values);
+  }
+
+  const fields = form.getValues().conditions.map((_, index) => (
+    <Grid.Col span={{ md: 12, lg: 6, xl: 4 }} key={conditions[index].id}>
+      <PatientConditionCard
+        index={index}
+        condition={conditions[index].condition}
+        form={form}
+        editing={editing}
+      />
+    </Grid.Col>
+  ));
+
+  return (
+    <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+      <Grid gutter="xs">{fields}</Grid>
+      {editing && (
+        <Flex
+          justify="space-between"
+          align="center"
+          direction="row-reverse"
+          mt="sm"
+        >
+          <Group gap="xs">
+            <Button variant="default" fw="normal" onClick={handleCancel}>
+              Cancelar
+            </Button>
+            <Button type="submit" loading={mutation.isPending}>
+              Salvar
+            </Button>
+          </Group>
+        </Flex>
+      )}
+    </form>
+  );
+}
