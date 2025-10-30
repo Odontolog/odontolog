6943b5010860fe631a31e457c7fa0d9790dd1a77
Arthur Soares <arthurpmrs@gmail.com>
2025-10-30 17:43:59 -0300

    fix: change default redirect route

--- frontend/src/shared/routes.ts
+++ frontend/src/shared/routes.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_REDIRECT = '/';
+export const DEFAULT_REDIRECT = '/patients';
 export const AUTH_ROUTES = ['/login', '/register', '/change-password'];
 export const SUPERVISOR_ROUTES = ['/students', '/validations'];
 export const NOT_FOUND = '/not-found';
