b93c6a27f1dbadd10eb81ba50d434366d757a695
BeaRoodriguees <beatrizroodriguees@hotmail.com>
2025-10-30 00:02:50 -0300

    feat: Add StudentProcedureHistorySection to StudentPage and create procedures history component

--- frontend/src/features/student/student.tsx
+++ frontend/src/features/student/student.tsx
@@ -3,6 +3,7 @@ import StudentHeader from './header';
 import { ScrollArea, Stack } from '@mantine/core';
 import PatientsSection from '../patients/patients';
 import { getAllPatients } from '../appshell/requests';
+import StudentProcedureHistorySection from './procedures-history';
 
 interface StudentPageProps {
   student: Student;
@@ -16,8 +17,9 @@ export default async function StudentPage({ student, user }: StudentPageProps) {
     <>
       <StudentHeader student={student} user={user} />
       <ScrollArea w="100%" flex={1}>
-        <Stack px="lg" py="md">
+        <Stack gap="lg" px="lg" py="md">
           <PatientsSection patients={patients} />
+          <StudentProcedureHistorySection />
         </Stack>
       </ScrollArea>
     </>
