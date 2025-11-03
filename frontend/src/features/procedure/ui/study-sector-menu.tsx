cd7cdcc7a85e861b9496d8e462a002ecf333617d
Arthur Soares <arthurpmrs@gmail.com>
2025-11-03 15:48:15 -0300

    fix: study sector menu position

--- frontend/src/features/procedure/ui/study-sector-menu.tsx
+++ frontend/src/features/procedure/ui/study-sector-menu.tsx
@@ -30,6 +30,7 @@ export default function StudySectorMenu({
     <Menu
       withinPortal
       position="bottom"
+      floatingStrategy="fixed"
       shadow="sm"
       opened={menuOpened}
       onChange={setMenuOpened}
